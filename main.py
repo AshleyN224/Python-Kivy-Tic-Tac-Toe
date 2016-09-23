eb7969d8d48cb1b87d822cdb1b4e92a4fde3c452
Katsar0v <me@katsarov.info>
2016-09-23 22:40:04 +0200

    update

--- main.py
+++ main.py
@@ -10,31 +10,31 @@ from random import randint
 class TicTacToe(App):
 
     title = 'Tic Tac Toe'
-    states = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    buttons = []
     choices = ["X","O"]
 
     def build(self):
         Config.set('graphics', 'width', '600')
         Config.set('graphics', 'height', '600')
-        layout = StackLayout()
+        self.layout = StackLayout()
         for x in range(9): # range() explanation: http://pythoncentral.io/pythons-range-function-explained/
-            bt = Button(text=' ', font_size=200, width=200, height=200, size_hint=(None, None), id=str(x+1))
+            bt = Button(text=' ', font_size=200, width=200, height=200, size_hint=(None, None), id=str(x))
             bt.bind(on_release=self.btn_pressed)
-            layout.add_widget(bt)
-        return layout
+            self.buttons.append(bt)
+            self.layout.add_widget(bt)
+        return self.layout
 
     def on_start(self):
         self.init_players();
         greeting = "Hello Player! You are playing with \"" + self.player + "\""
-        popup = Popup(title="Welcome!",
-                content=Label(text=greeting),
-                size=(300, 100),
-                size_hint=(None, None))
+        popup = Popup(title="Welcome!", content=Label(text=greeting), size=(300, 100), size_hint=(None, None))
         popup.open()
 
     def btn_pressed(self, button):
         button.text="X"
-        self.states[int(button.id)-1] = "X"
+        for button in self.buttons:
+            print(button.id + ": " + button.text)
+        print(" ")
 
     def init_players(self):
         rand_choice = randint(0,1);
